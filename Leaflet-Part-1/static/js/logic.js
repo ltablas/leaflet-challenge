bb22d90ac5b476f7b9a50aa33a29bae075cffade
ltablas <luciatablas@gmail.com>
2023-03-27 13:32:01 -0700

    uploading starter files

---
+++ Leaflet-Part-1/static/js/logic.js
@@ -0,0 +1,9 @@
+// var url = https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson
+
+// var map = L.map('map').setView([40.7128, -74.0060], 10);
+
+// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     maxZoom: 19,
+//     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+// }).addTo(map);
+
